96d8433fc97e84ac0bb92562ea1ab9a7e8a6b8e4
Nikhilesh Bisht <nikkihotshot@gmail.com>
2025-09-10 00:25:57 +0530

    icon at tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,7 +79,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${montserrat.variable} scroll-smooth`}>
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.svg" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
       </head>
