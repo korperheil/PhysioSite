ff81d01d05d2c3bdf66898538314099f81c765cc
Nikhilesh Bisht <nikkihotshot@gmail.com>
2025-09-10 00:50:41 +0530

    icon at google

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,8 +79,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${montserrat.variable} scroll-smooth`}>
       <head>
-        <link rel="icon" href="/logo.svg" />
-        <link rel="apple-touch-icon" href="/logo.svg" />
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
       </head>
       <body className={montserrat.className}>
